b4fbfecbcca765a29246cd9a56c4ec568d7dd991
Jakob Willforss <jakob.willforss@hotmail.com>
2017-03-15 14:43:03 +0100

    Re-add untracked template for Git-reference to evaluationProcessor file

--- R/EvaluationUtils/evaluationProcessor.R
+++ R/EvaluationUtils/evaluationProcessor.R
@@ -117,7 +117,51 @@ screen_values <- function(sig_thresholds, do_fdr, rt_windows, window_shifts,
 }
 
 
-
+# hardcoded_screen_values <- function(do_full_run, super_dirname, subset=T, debug=F) {
+#     
+#     if (do_full_run) {
+#         sig_thres <- c(0.1, 0.05)
+#         do_fdr <- c(TRUE)
+#         # rt_windows <- c(1,2)
+#         rt_windows <- c(seq(1,20,2))
+#         frame_shifts <- c(1, 3, 5)
+#         lowest_window_size <- c(100, 200, 500)
+#         window_merge_method <- c("median")
+#         max_cores <- 7
+#         target_replicates <- list(c(2,3))
+#         stat_test <- c("welch")
+#         quiet <- FALSE
+#     }
+#     else {
+#         sig_thres <- c(0.1)
+#         do_fdr <- c(TRUE)
+#         # rt_windows <- c(seq(0.5, 5, 0.5), seq(6, 15, 1))
+#         rt_windows <- c(10, 30, 50, 100)
+#         frame_shifts <- c(1)
+#         lowest_window_size <- c(200)
+#         window_merge_method <- c("median")
+#         max_cores <- 6
+#         target_replicates <- list(c(2,3))
+#         stat_test <- c("welch")
+#         quiet <- FALSE
+#         
+#         verbose <- TRUE
+#     }
+#     
+#     screen_values(sig_thresholds = sig_thres,
+#                   do_fdr = do_fdr,
+#                   rt_windows = rt_windows,
+#                   window_shifts = frame_shifts,
+#                   lowest_window_size = lowest_window_size,
+#                   window_merge_method = window_merge_method,
+#                   max_cores = max_cores,
+#                   quiet_processing = quiet,
+#                   subset = subset,
+#                   super_dirname = super_dirname,
+#                   sample_comparisons = target_replicates,
+#                   stat_test = stat_test,
+#                   debug = debug)
+# }
 
 
 
